350dd03b6bcd3633a0f480940643d0df83831a95
jisoo <doh_0112@naver.com>
2023-05-06 00:41:18 +0900

    Header 컴포넌트 생성

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>LOGO</h1>
+      {/* 기본 상태 - 회원가입/로그인 */}
+      {/* 로그인 상태 - 유저 정보 */}
+    </header>
+  );
+};
+
+export default Header;
