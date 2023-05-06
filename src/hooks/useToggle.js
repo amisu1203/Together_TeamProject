943ac0d31607c33bd240319d951e91857401fd8d
jisoo <doh_0112@naver.com>
2023-05-07 08:00:48 +0900

    useToggle 훅 생성

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const useToggle = () => {
+  const [value, setValue] = useState(false);
+
+  const handler = () => {
+    setValue((value) => !value);
+  };
+
+  return [value, handler, setValue];
+};
+
+export default useToggle;
