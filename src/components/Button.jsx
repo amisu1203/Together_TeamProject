2f2bf4d39ce18084b70aee19e3c99822ed08b815
jisoo <doh_0112@naver.com>
2023-05-07 07:53:16 +0900

    버튼 prop 중 type에 대해서 조건부 스타일링 및 조건부 타입 지정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 
-const Button = ({ button, onClick, isDisabled }) => {
-  return button.purpose === "more info" ? <StBtn onClick={onClick}>{button.title}</StBtn> : <button onClick={onClick}>{button.title}</button>;
+const Button = ({ button, onClickHandle, isDisabled }) => {
+  return button.purpose === "more info" ? (
+    <StBtn type="button" onClick={onClickHandle}>
+      {button.title}
+    </StBtn>
+  ) : (
+    <button type={button.type || "button"} onClick={onClickHandle}>
+      {button.title}
+    </button>
+  );
 };
 
 export default Button;
 
 const StBtn = styled.button`
+  /* display: inline-block; */
+  width: 100%;
   border: none;
   border-bottom: 1px solid black;
   background-color: transparent;
+  padding: 10px 0;
 `;
