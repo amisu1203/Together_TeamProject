ab03b5c5881ef300cc00905db7d79b540aab6aea
jisoo <doh_0112@naver.com>
2023-05-06 15:24:19 +0900

    로고 사진 연결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../img/logo.png";
 
 const Header = () => {
   return (
-    <header>
-      <h1>LOGO</h1>
+    <StHeader>
+      <StLogo>LOGO</StLogo>
       {/* 기본 상태 - 회원가입/로그인 */}
       {/* 로그인 상태 - 유저 정보 */}
-    </header>
+    </StHeader>
   );
 };
 
 export default Header;
+
+const StHeader = styled.header`
+  margin-bottom: 90px;
+  height: 100px;
+`;
+
+const StLogo = styled.h1`
+  background-image: url(${logo});
+  background-size: cover;
+  width: 200px;
+  height: 200px;
+  text-indent: -9999px;
+  position: relative;
+  top: -30px;
+  margin: 0 auto;
+`;
