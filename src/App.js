2b24ceecea17102ca2ab75029cfce7683cf58eb4
jisoo <doh_0112@naver.com>
2023-05-06 15:22:37 +0900

    폰트 연결

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
+import FormConsultPage from "./pages/FormConsultPage";
+import styled from "styled-components";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/api/detail/:id" element={<DetailPage />} />
-      </Routes>
-    </BrowserRouter>
+    <StContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/api/detail/:id" element={<DetailPage />} />
+          <Route path="/api/consulting" element={<FormConsultPage />} />
+        </Routes>
+      </BrowserRouter>
+    </StContainer>
   );
 }
+
+const StContainer = styled.div`
+  margin: 0 auto;
+  max-width: 1200px;
+  text-align: center;
+  font-family: "Noto Sans KR", sans-serif;
+`;
 
 export default App;
