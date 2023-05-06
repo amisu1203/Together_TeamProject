fc1edad7cf782c732410d6e3b2ff585b2c830037
jisoo <doh_0112@naver.com>
2023-05-07 07:52:03 +0900

    로고 스타일링, 클릭시 홈페이지 이동 로직 추가

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import logo from "../img/logo.png";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import "animate.css";
 
 const Logo = () => {
-  return <StLogo>LOGO</StLogo>;
+  const navigate = useNavigate();
+  // 홈페이지 이동 핸들러
+  const HandleLogoClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <StHomeLogo onClick={HandleLogoClick} className="animate__heartBeat">
+      LOGO
+    </StHomeLogo>
+  );
 };
 
 export default Logo;
 
-const StLogo = styled.h1`
+const StHomeLogo = styled.h1`
   background-image: url(${logo});
   background-size: cover;
-  width: 180px;
-  height: 170px;
+  width: 130px;
+  height: 140px;
   text-indent: -9999px;
+  position: relative;
+  top: -20px;
+  cursor: pointer;
 `;
+
+// const StSideLogo = styled.h1`
+//   background-image: url(${logo});
+//   background-size: cover;
+//   width: 180px;
+//   height: 170px;
+//   text-indent: -9999px;
+//   position: relative;
+//   top: -20px;
+//   left: -40%;
+//   cursor: pointer;
+// `;
