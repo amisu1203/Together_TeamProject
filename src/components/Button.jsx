83ea02ab282c4a13458f48c87ca5cce7582336de
jisoo <doh_0112@naver.com>
2023-05-06 15:23:20 +0900

    버튼 더보기일때 스타일 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
-const Button = ({ button }) => {
-  return <div>{button.title}</div>;
+const Button = ({ button, onClick, isDisabled }) => {
+  return button.purpose === "more info" ? <StBtn onClick={onClick}>{button.title}</StBtn> : <button onClick={onClick}>{button.title}</button>;
 };
 
 export default Button;
+
+const StBtn = styled.button`
+  border: none;
+  border-bottom: 1px solid black;
+  background-color: transparent;
+`;
