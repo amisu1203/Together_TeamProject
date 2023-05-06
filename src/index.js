1a43b12396c5151237c1648a3e6f70c463cb96d1
jisoo <doh_0112@naver.com>
2023-05-06 15:23:07 +0900

    배경 그라데이션 추가

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/config/configStore";
+import styled from "styled-components";
+
+const StContainer = styled.div`
+  background: linear-gradient(rgb(252 212 188 / 61%), rgb(255 172 87 / 33%), rgb(255, 255, 255) 17%);
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <StContainer>
+      <App />
+    </StContainer>
   </Provider>
 );
