1062a1da661a1c76f4cb8147422f71542f67f51a
jisoo <doh_0112@naver.com>
2023-05-08 10:51:54 +0900

    applyForm 리듀서에 추가

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import animalList from "../modules/animalListSlice";
+import applyForm from "../modules/applyFormSlice";
 
 const store = configureStore({
   reducer: {
     animalList,
+    applyForm,
   },
 });
 
