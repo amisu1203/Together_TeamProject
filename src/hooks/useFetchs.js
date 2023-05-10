921511331fd5fd7c05bd316ab18852090fc109ba
jisoo <doh_0112@naver.com>
2023-05-10 21:24:54 +0900

    useGetList에서 axios response 값 받는 변수 변경

--- src/hooks/useFetchs.js
+++ src/hooks/useFetchs.js
@@ -5,21 +5,19 @@ import { setAnimalList } from "../redux/modules/animalListSlice";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
+  responseType: "json",
 });
 export const useGetList = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const getListOfAnimal = async () => {
       try {
-        // const { data: listOfAnimal } = await axiosInstance.get("/api/list");
-        const data = await axiosInstance.get("/api/list");
-        console.log(data);
+        const { data: listOfAnimal } = await axiosInstance.get("/api/list/");
         // 공공 api 사용시 요청 로직
         // const { data } = await axiosInstance.get("/");
         // const listOfAnimal = data.TbAdpWaitAnimalView.row;
 
-        // dispatch(setAnimalList(listOfAnimal));
-        dispatch(setAnimalList(data));
+        dispatch(setAnimalList(listOfAnimal));
       } catch (error) {
         console.error(error);
       }
