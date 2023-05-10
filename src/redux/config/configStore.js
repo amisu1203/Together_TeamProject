d6d819f6a1488aa9c5d414462ecb84dbc306530d
jisoo <doh_0112@naver.com>
2023-05-10 21:25:13 +0900

    auth slice 추가

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import animalList from "../modules/animalListSlice";
 import applyForm from "../modules/applyFormSlice";
+import auth from "../modules/auth";
 
 const store = configureStore({
   reducer: {
     animalList,
     applyForm,
+    auth,
   },
 });
 
